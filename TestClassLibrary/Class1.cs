c9fc7d7bc08271bf790a6a47274294e3ef937f95
tianjunfei <tianjunfei@163.com>
2021-04-19 11:17:09 +0800

    天霸

--- TestClassLibrary/Class1.cs
+++ TestClassLibrary/Class1.cs
@@ -5,6 +5,10 @@ namespace TestClassLibrary
     public class Class1
     {
         public int MyProperty { get; set; }
+        public int dd { get; set; }
+        public int fff { get; set; }
+        public int ggg { get; set; }
+        public int uuuj { get; set; }
 
     }
 }
